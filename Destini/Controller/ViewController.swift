ea6d763f248594cc735140cb457425722dc0dcde
Ilyas Tyumenev <tumenoff@mail.ru>
2023-04-14 14:09:42 +0300

    Update the Story

--- Destini/Controller/ViewController.swift
+++ Destini/Controller/ViewController.swift
@@ -17,16 +17,30 @@ class ViewController: UIViewController {
                    Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
                    Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")]
     
+    var choiceNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        storyLabel.text = stories[0].title
-        choice1Button.setTitle(stories[0].choice1, for: .normal)
-        choice2Button.setTitle(stories[0].choice2, for: .normal)
-        
+                
+        updateUI(choiceNumber)
     }
 
     @IBAction func choiceMade(_ sender: UIButton) {
+        
+        let userChoice = sender.currentTitle
+        
+        if userChoice == stories[choiceNumber].choice1 {
+            choiceNumber += 1
+        } else {
+            choiceNumber += 2
+        }
+        updateUI(choiceNumber)
+    }
+    
+    func updateUI(_ number: Int) {
+        storyLabel.text = stories[number].title
+        choice1Button.setTitle(stories[number].choice1, for: .normal)
+        choice2Button.setTitle(stories[number].choice2, for: .normal)
     }
     
 }
